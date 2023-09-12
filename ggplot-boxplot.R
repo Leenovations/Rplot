2baa37ea8d58ed9048c681535b517600d1b20315
Trevor lee <142461691+Leenovations@users.noreply.github.com>
2023-09-12 15:42:47 +0900

    Update ggplot-boxplot.R

--- ggplot-boxplot.R
+++ ggplot-boxplot.R
@@ -2,17 +2,21 @@ library(ggpubr)
 library(rstatix)
 library(ggplot2)
 #------------------------------------------------#
-Date <- Sys.Date()
-Date <- format(today, "%y%m%d")
+Data <- read.table('',
+                   sep='\t',
+                   header=T)
+
+Gene <- unique(Data$Gene)
 #------------------------------------------------#
 for (gene in Gene){
-  Sub <- subset(Total, Gene==gene)
+  Sub <- subset(Data, Gene==gene)
   
-  bxp <- ggplot(Sub, aes(x=Group, y=Methylation, color=Group)) + 
+  bxp <- ggplot(Sub, aes(x=Group, y=Meth, color=Group)) + 
     geom_boxplot(lwd = 0.6, width = 0.5, outlier.shape = NA) +
     scale_color_manual(values=c("tan1", "coral", "lightskyblue")) +
     geom_jitter(width=0.15, alpha=0.5) +
     theme_classic() +
+    scale_y_continuous(breaks = c(0, 25, 50, 75, 100)) +
     theme(axis.title = element_text(size = 15),
           plot.title = element_text(size = 15, hjust = 0.5, face='italic'),
           legend.text = element_text(size = 10),
@@ -27,5 +31,5 @@ for (gene in Gene){
     ggtitle(sprintf('%s', gene)) +
     xlab('') +
     ylab('Percent Methylation')
-  ggsave(paste0(Path, Date,'.', gene, '.Plot.pdf'))
+  ggsave(paste0(gene, '.boxplot.pdf'))
 }
